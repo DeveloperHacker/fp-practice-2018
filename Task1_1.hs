6492954db0a8655d02a29f89a8522c7dee6cca74
Sergei Vorobyov <hacker.mad.cat@gmail.com>
2018-09-21 15:29:04 +0300

    added solution for task '1-1'

--- Task1_1.hs
+++ Task1_1.hs
@@ -1,27 +1,55 @@
+{-# LANGUAGE ViewPatterns #-}
 module Task1_1 where
 
 import Todo(todo)
 
+data Operation = Plus | Minus | Times 
+    deriving(Show, Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
-            deriving(Show,Eq)
+            | BinaryTerm{ op :: Operation, lhv :: Term, rhv :: Term } -- бинарная операция
+            deriving(Show, Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm Plus l r
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm Minus l r
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm Times l r
+infixl 6 |+|
+infixl 6 |-|
+infixl 7 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression = case (expression) of
+    Variable var | var == varName -> replacement
+    BinaryTerm op lhv rhv -> BinaryTerm op (replaceVar varName replacement lhv) (replaceVar varName replacement rhv)
+    othervise -> expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate expression = case (expression) of 
+    Variable _ -> expression
+    IntConstant _ -> expression
+    BinaryTerm Plus (evaluate -> l) (evaluate -> r) -> case (l, r) of 
+        (IntConstant a, IntConstant b) -> IntConstant $ a + b
+        (IntConstant 0, _) -> r
+        (_, IntConstant 0) -> l
+        othervise -> l
+    BinaryTerm Minus (evaluate -> l) (evaluate -> r) -> case (l, r) of 
+        (IntConstant a, IntConstant b) -> IntConstant $ a - b
+        (_, IntConstant 0) -> l
+        othervise -> l
+    BinaryTerm Times (evaluate -> l) (evaluate -> r) -> case (l, r) of 
+        (IntConstant a, IntConstant b) -> IntConstant $ a * b
+        (IntConstant 0, _) -> IntConstant 0
+        (_, IntConstant 0) -> IntConstant 0
+        (IntConstant 1, _) -> r
+        (_, IntConstant 1) -> l
+        othervise -> l
--- main.hs
+++ main.hs
@@ -1,10 +1,32 @@
 module Main where
 
 import Prelude hiding (sin, cos, gcd)
+import Task1_1 (Term(IntConstant, Variable))
+import Task1_1 (Term, (|+|), (|-|), (|*|), replaceVar, evaluate)
 import Task1_2 (sin, cos, pow, isPrime, gcd)
 
-main :: IO ()
-main = do
+test1_1 = 
+    let 
+        expr1 = (Variable "a") |+| Variable "b" |*| Variable "c"
+        expr2 = Variable "a" |-| Variable "b" |-| Variable "c"
+        expr3 = Variable "a" |-| Variable "b" |*| IntConstant 10 |-| Variable "c"
+        expr4 = expr1 |*| expr2 |+| expr3
+        value1 = replaceVar "a" (IntConstant 2) $ replaceVar "b" (IntConstant 3) $ replaceVar "c" (IntConstant 4) expr1
+        value2 = replaceVar "a" (IntConstant 2) $ replaceVar "c" (IntConstant 0) expr1
+        value3 = replaceVar "a" (IntConstant 2) $ replaceVar "b" (IntConstant 3) $ replaceVar "c" (IntConstant 4) expr2
+        value4 = replaceVar "a" (IntConstant 2) $ replaceVar "b" (IntConstant 3) $ replaceVar "c" (IntConstant 4) expr3
+        value5 = replaceVar "a" (IntConstant 2) $ replaceVar "b" (IntConstant 3) $ replaceVar "c" (IntConstant 4) expr4
+    in
+    do
+        putStrLn "Test1-1"
+        putStrLn (show $ evaluate value1)
+        putStrLn (show $ evaluate value2)
+        putStrLn (show $ evaluate value3)
+        putStrLn (show $ evaluate value4)
+        putStrLn (show $ evaluate value5)
+
+test1_2 = do
+    putStrLn "Test1-2"
     putStrLn (show (pow (-1) 10))
     putStrLn (show (pow 3 3))
     putStrLn (show (sin 0.78539816339))
@@ -31,3 +53,9 @@ main = do
     putStrLn (show (gcd 80 20))
     putStrLn (show (gcd 30 18))
     putStrLn (show (gcd 18 30))
+
+main :: IO ()
+main = do
+    test1_1
+    putStrLn ""
+    test1_2
